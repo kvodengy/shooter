b683f5ac936e2e416bb55c2a7401e2bee9bedfeb
kvodengy <149088432+kvodengy@users.noreply.github.com>
2024-04-04 18:41:22 +0300

    Add files via upload

--- shooter.py
+++ shooter.py
@@ -1,12 +1,16 @@
 import pygame
 from random import randint
+import time
+
 pygame.font.init()
 pygame.mixer.init()
 
 FPS = pygame.time.Clock()
-win_width = 1280
-win_height = 665
+win_width = 1380
+win_height = 700
 p_size = 100
+enemies = []
+bullets = []
 
 window = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Шутер")
@@ -21,48 +25,88 @@ class Settings():
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(Settings):
+    
     def __init__(self, image, x, y, w, h, s):
         super().__init__(image, x, y, w, h)
         self.speed = s
+        self.bullets = []
+        self.hp = 3
+        self.image_hp = pygame.transform.scale(pygame.image.load("heart.png"), (100,100))
+
+    def draw_hp(self):
+        for h in range(self.hp):
+            window.blit(self.image_hp, (h*100,0))
 
     def move(self):
+        self.draw_hp()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.rect.x>0:
             self.rect.x -= self.speed
         if keys[pygame.K_RIGHT] and self.rect.x<win_width-self.rect.width:
             self.rect.x += self.speed 
-    def crash(self, l):
-        for w in l:
-            if self.rect.colliderect(w.rect):
-                return True
-            else:
-                return False
+
+    def bulletshoot(self):
+        global shoot
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and shoot:
+            shoot = False
+            bullets.append(Bullet("bullet.png", self.rect.centerx-20, self.rect.centery//1.25, 50, 100, 20))
+
+    
             
 class Enemy(Player):
     def __init__(self, image, x, y, w, h, s):
         super().__init__(image, x, y, w, h, s)
-        self.direction = True #right
 
-    def move(self, amount):
-        i = 0
+    def move(self):
+        global player, enemies
         self.rect.y += self.speed
-        if self.rect.y>670:
-            self.rect.x = randint(0,win_width)
-            self.rect.y = -20
-            i = i + 1
+        if self.rect.colliderect(player.rect) and self in enemies:
+            player.hp -= 1
+            enemies.remove(self)
+        if self.rect.y>win_height-p_size:
+            self.rect.x = randint(0,win_width-p_size)
+            self.rect.y = randint(-10*p_size, -1*p_size)
+        for e in enemies:
+            if e != self and self.rect.colliderect(e.rect):
+                self.rect.x = randint(0,win_width-p_size)
+                self.rect.y = randint(-10*p_size, -1*p_size)
 
-    #def move2(self):
-        #self.rect.y += self.speed
 
+def level1():
+    global enemies, num_enemies, start, num_level
+    if start == True and num_level==1:
+        num_enemies = 5
+        i = 0
+        if i < 3:
+            for enemy in range(num_enemies):
+                enemies.append(Enemy("alien.png", randint(0,win_width), randint(-5*p_size, -1*p_size), p_size, p_size, 5))
+                i += 1
+                start = False
 
+class Bullet(Player):
+    def __init__(self, image, x, y, w, h, s):
+        super().__init__(image, x, y, w, h, s)
+
+    def move(self):
+        global enemies, bullets
+        self.rect.y -= self.speed
+        if self.rect.y < -10:
+            bullets.remove(bullet)
+        for enemy in enemies:
+            if self.rect.colliderect(enemy.rect) and self in bullets:
+                enemies.remove(enemy)
+                bullets.remove(bullet)
 
 bg = Settings("background.png", 0, 0, win_width, win_height)
-player = Player("rocket.png", win_width//2.05, win_height//1.25, p_size, p_size, 10)
-enemy = Enemy("alien.png", randint(0,win_width), 0, p_size, p_size, 10)
-#boss = Enemy("alien.png", randint(0,win_width), 0, p_size*2, p_size*2, 5)
+player = Player("rocket.png", win_width//2.05, win_height//1.25, p_size, p_size, 20) 
 
+finish = False
+start = True
+num_level = 1
 game = True
-i = 1
+current = time.time()
+shoot = True
 
 
 pygame.mixer.music.load("bgmusic.mp3")
@@ -72,13 +116,30 @@ while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    bg.draw()
-    player.draw()
-    player.move()
-    enemy.draw()
-    enemy.move(3)
-    #boss.draw()
-    #boss.move2()
+    
+    if finish != True:
+        bg.draw()
+        player.draw()
+        player.move()
+        player.bulletshoot()
+        
+        level1()
 
+        if time.time()-current>0.5:
+            current = time.time()
+            shoot = True
+
+    
+        for enemy in enemies:
+            enemy.move()
+            enemy.draw()
+
+        for bullet in bullets:
+            bullet.move()
+            bullet.draw()
+
+        if player.hp <= 0:
+            finish = True
+    
     pygame.display.flip()
     FPS.tick(60)
